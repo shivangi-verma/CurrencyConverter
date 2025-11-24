e6852c8e39ebf28ccc0dd654129af094e566336a
Shivangi <81459301+shivangi-verma@users.noreply.github.com>
2025-11-24 22:31:25 +0530

    fixed bugs in useCurrencyInfo hook

--- src/Components/InputBox.jsx
+++ src/Components/InputBox.jsx
@@ -1,3 +1,5 @@
+import { React, useId } from "react";
+
 function InputBox({
   label,
   amount,
@@ -9,11 +11,18 @@ function InputBox({
   onCurrencyChange,
   className = "",
 }) {
+  const amountInputId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
         <input
+          id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"

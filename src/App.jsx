7873e726ae31ba2e9374152140d58161b884e812
Shivangi <81459301+shivangi-verma@users.noreply.github.com>
2025-11-24 22:36:58 +0530

    minor css changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import useCurrencyInfo from "./hooks/useCurrencyInfo";
 // import "./App.css";
 
 function App() {
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState();
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("inr");
-  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState();
 
   const currencyInfo = useCurrencyInfo(from);
 
--- src/Components/InputBox.jsx
+++ src/Components/InputBox.jsx
@@ -13,7 +13,7 @@ function InputBox({
 }) {
   const amountInputId = useId();
   return (
-    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+    <div className={`bg-white p-3 rounded-lg py-4 text-sm flex ${className}`}>
       <div className="w-1/2">
         <label
           htmlFor={amountInputId}
@@ -23,7 +23,7 @@ function InputBox({
         </label>
         <input
           id={amountInputId}
-          className="outline-none w-full bg-transparent py-1.5"
+          className="bg-gray-100 rounded-md w-full px-1 py-1 "
           type="number"
           placeholder="Amount"
           disabled={amountDisable}
@@ -36,7 +36,7 @@ function InputBox({
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
         <select
-          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          className="rounded-lg px-1.5 py-1 bg-gray-600 text-white cursor-pointer outline-none"
           value={selectCurrency}
           onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
           disabled={currencyDisable}

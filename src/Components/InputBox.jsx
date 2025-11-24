61e64ea20bfb116bd3341e9b670fff25d51dd867
Shivangi <81459301+shivangi-verma@users.noreply.github.com>
2025-11-24 21:24:48 +0530

    feat: add InputBox component and implement useCurrencyInfo hook for currency data fetching

--- src/Components/InputBox.jsx
+++ src/Components/InputBox.jsx
@@ -1,10 +1,10 @@
 function InputBox({
   label,
-
+  
   className = "",
 }) {
   return (
-    <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
         <label className="text-black/40 mb-2 inline-block">label</label>
         <input
